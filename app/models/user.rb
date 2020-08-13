1b6e5eec21ab9222cf52226da01e49d838e9595c
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-08-13 19:26:57 +0900

    userモデルにバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  zenkaku = /\A[ぁ-んァ-ン一-龥]/
+  kana = /\A[ァ-ヶー－]+\z/
+       
+with_options presence: true do
+  validates :nickname
+  validates :password, length: { minimum: 6 },
+            format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
+  validates :family_name_full_width, format: { with: zenkaku, message: "is invalid. Input full-width characters."}
+  validates :first_name_full_width, format: { with: zenkaku, message: "is invalid. Input full-width characters."}
+  validates :family_name_kana_input, format: { with: kana, message: "is invalid. Input full-width katakana characters."}
+  validates :first_name_kana_input, format: { with: kana, message: "is invalid. Input full-width katakana characters."}
+  validates :birthday
+  end
 end
