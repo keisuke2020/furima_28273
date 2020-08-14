e24ae3df888dc79ca37b01fd7619d28aa8158c6a
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-08-14 14:19:02 +0900

    itemモデルのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :charge, :delivery, :origin, :status
+  
+  with_options presence: true do
+    validates :exhibition_image
+    validates :name,                length: { maximum: 40 }
+    validates :product_explanation, length: { maximum: 1000 }
+    validates :price,               numericality: { :greater_than => 300 }, { :less_than_or_equal_to => 9999999 } 
+  end
+
+  with_options numericality: { other_than: 1 }
+    validates:category_id, presence: true
+    validates:charge_id,   presence: true
+    validates:delivery_id, presence: true
+    validates:origin_id,   presence: true
+    validates:status_id,   presence: true
+  end
+
   belongs_to :user
   has_one    :purchase
   has_one    :address
