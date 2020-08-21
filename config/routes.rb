6b09729f4f4ff19cef209d23c8b53ad56119151b
keisuke2020 <kei_a_study@yahoo.co.jp>
2020-08-21 19:18:31 +0900

    ルーティングを設定&商品購入ページのビューファイルを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   root to: "items#index"
-  resources :items
-  resources :transactions
+  resources :items do
+    resources :transactions
+  end
 end
